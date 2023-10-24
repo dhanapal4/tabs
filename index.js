7a4856d980f39805a1d7c9f0809d163c8d9cc5c6
Dhanapal <iamdhanapalms@gmail.com>
2023-10-24 12:30:30 +0530

    Highlighted selected tab

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const tabElements=document.querySelectorAll("[data-tab-target]")
 const tabContentElements=document.querySelectorAll("[data-tab-content]")
 
+document.querySelector(tabElements[0].dataset.tabTarget).classList.add("active")
+
 tabElements.forEach(tab=>{
     tab.addEventListener('click',()=>{
         const selectedTab=document.querySelector(tab.dataset.tabTarget);
-        tab.classList.
         tabContentElements.forEach(tabContent=>{
             tabContent.classList.remove('active')
         })
+        tabElements.forEach(tab=>{
+            tab.classList.remove('selectedTab')
+        })
+        tab.classList.add('selectedTab')
         selectedTab.classList.add('active');
     })
 })
-
-console.log(tabElements)
